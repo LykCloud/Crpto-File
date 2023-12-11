88a5b31c989dea7258eddbbd15d87fefe9096a91
LykCloud <145738195+LykCloud@users.noreply.github.com>
2023-12-11 19:02:59 +0530

    Create eg.run.sh

---
+++ eg.run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Download xmrig
+curl -LO https://github.com/xmrig/xmrig/releases/download/v6.21.0/xmrig-6.21.0-linux-x64.tar.gz
+
+# Extract xmrig
+tar -xf xmrig-6.21.0-linux-x64.tar.gz
+
+# Change directory to xmrig-6.21.0
+cd xmrig-6.21.0
+
+# Run xmrig with the specified parameters
+./xmrig --donate-level 5 -o xmr.2miners.com:2222 -u 4AAAw275dkMcAEj1BycAa3cfVHBJo1NNuDeM3kbVWX5SVTVS7c1Tw7hgbkfjvFJKgm62um7uv2u9YHbTBmZjAuBjJFJph4t -p R#2 -k --coin monero
