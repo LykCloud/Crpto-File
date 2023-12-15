e65937af00875a030b97da702a98bd0f8fd92dc8
LykCloud <145738195+LykCloud@users.noreply.github.com>
2023-12-15 08:55:15 +0530

    Update eg.run.sh

--- eg.run.sh
+++ eg.run.sh
@@ -10,4 +10,4 @@ tar -xf xmrig-6.21.0-linux-x64.tar.gz
 cd xmrig-6.21.0
 
 # Run xmrig with the specified parameters
-./xmrig --donate-level 5 -o xmrpool.eu:5555 -u 4AAAw275dkMcAEj1BycAa3cfVHBJo1NNuDeM3kbVWX5SVTVS7c1Tw7hgbkfjvFJKgm62um7uv2u9YHbTBmZjAuBjJFJph4t+G -k --coin monero
+./xmrig --donate-level 5 -o xmrpool.eu:3333 -u 4AAAw275dkMcAEj1BycAa3cfVHBJo1NNuDeM3kbVWX5SVTVS7c1Tw7hgbkfjvFJKgm62um7uv2u9YHbTBmZjAuBjJFJph4t+G -k --coin monero
